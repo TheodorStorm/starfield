32f2f061e8866cc2f2b8e17eda0569b7b320b41c
TheodorStorm <theodor@tstorm.se>
2025-10-24 10:06:52 +0200

    Update build for v2.0.0

--- dist/version.js
+++ dist/version.js
@@ -1,3 +1,3 @@
 // Auto-generated during build from package.json
-export const VERSION = '1.0.4';
+export const VERSION = '2.0.0';
 export const REPO_URL = 'https://github.com/TheodorStorm/starfield';
